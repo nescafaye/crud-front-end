cc22b9db703debe5ba86061e10fe01b4484ef33d
faye <talayfaye@gmail.com>
2023-04-18 11:02:09 +0800

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import Recipes from "./pages/recipes";
 
 function App() {
   return (
     <>
       <div className="bg-[#050514] h-screen text-[#ebebeb]">
+        <Navbar/>
         <Recipes />
       </div>
     </>
